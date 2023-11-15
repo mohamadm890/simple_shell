4796bd10955423b2558a4c5232811b378e5a2953
SALAHDDINEDKAKI <salahdkaki199@gmail.c>
2023-11-15 12:15:53 -0800

    betty style

--- split.c
+++ split.c
@@ -14,30 +14,30 @@ char **split(char *line) {
 	int buffer = INITIAL_BUFFER_SIZE;
 	int position = 0;
 	char **tokens;
-   char **temp;
-    char *token;
-    tokens = malloc(buffer * sizeof(char *));
-    if (!tokens) {
-        return NULL;
-    }
-    token = strtok(line, " \t\r\n\a");
-    while (token != NULL) {
-        tokens[position] = token;
-        position++;
+	char **temp;
+	char *token;
+	tokens = malloc(buffer * sizeof(char *));
+	if (!tokens) {
+	return NULL;
+	}
+	token = strtok(line, " \t\r\n\a");
+	while (token != NULL) {
+		tokens[position] = token;
+		position++;
 
-        if (position >= buffer) {
-            buffer += BUFFER_INCREMENT; 
-            temp = realloc(tokens, buffer * sizeof(char *));
-            if (!temp) {
-                free(tokens);
-                return NULL;
-            }
-            tokens = temp;
-        }
+	if (position >= buffer) {
+		buffer += BUFFER_INCREMENT; 
+		temp = realloc(tokens, buffer * sizeof(char *));
+	    if (!temp) {
+		free(tokens);
+		return NULL;
+		}
+		tokens = temp;
+	}
 
-        token = strtok(NULL, " \t\r\n\a");
-    }
+	token = strtok(NULL, " \t\r\n\a");
+	}
 
-    tokens[position] = NULL;
-    return (tokens);
+	tokens[position] = NULL;
+	return (tokens);
 }
