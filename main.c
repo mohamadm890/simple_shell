c9291c0d560279e1896fe0b268f6fd65608bdfb2
SALAHDDINEDKAKI <salahdkaki199@gmail.c>
2023-11-16 07:48:37 -0800

    betty main.c

--- main.c
+++ main.c
@@ -1,52 +1,52 @@
 #include "shell.h"
 
-
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
-    char *line = NULL;
-    char **commands = NULL;
-    (void) argc;
-    while (1) {
-        
-        
-        line = getlineinput();
+	char *line = NULL;
+	char **commands = NULL;
+	(void) argc;
+	while (1)
+	{
+	line = getlineinput();
 
-        if (line == NULL) {
-            break;
-        }
+	if (line == NULL)
+	{
+		break;
+	}
 
-        commands = split(line);
-        if (!commands)
-        {
-	   free(line);
-           continue;
-        }
-         executeCommand(commands, argv);
-         free(line);
-	 free(commands);
+	commands = split(line);
+	if (!commands)
+	{
+		free(line);
+		continue;
+	}
+	executeCommand(commands, argv);
+	free(line);
+	free(commands);
 
-       
-    }
+	}
 
-    return 0;
+	return 0;
 }
 
+char *getlineinput(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t n;
 
-char *getlineinput(void) {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t n;
-    
-    if (isatty(STDIN_FILENO)) {
-        write(STDOUT_FILENO, "$ ", 2);
-    }
+	if (isatty(STDIN_FILENO))
+	{
+	write(STDOUT_FILENO, "$ ", 2);
+	}
 
-    n = getline(&line, &len, stdin);
+	n = getline(&line, &len, stdin);
 
-    if (n == -1) {
+	if (n == -1)
+	{
 	free(line);
-        return NULL;
-    }
+	return (NULL);
+	}
 
-    return line;
+	return (line);
 }
