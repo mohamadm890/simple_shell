fb47705e7d90ebcb0890fd31b59fd87c6037de51
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2023-11-15 23:38:21 -0800

    Delete shell.h

--- shell.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-int main(int argc, char **argv);
-int executeCommand(char **tokens, char **argv);
-char *getlineinput(void);
-char **split(char *line);
